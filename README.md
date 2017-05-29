e8a6da33fc46c6c42f7d1db3f8db13446faa5067
Arko <achakraborti1995@gmail.com>
2017-05-29 21:01:32 +0530

    Built graph ops

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # RNN-Tensorflow
-Learning to implement an various types of Recurrent Neural Network for modelling sequenced data
+Learning to implement an various types of Recurrent Neural Network for modelling sequenced data\
+A RNN(recurrent neural network) is a type of neural network that is good at understanding the time relation between data.
+
--- lstm_tf.py
+++ lstm_tf.py
@@ -22,6 +22,37 @@ class LSTM(object):
         self.vocab_size = vocab_size
         self.hidden_size = hidden_size
 
+        with tf.name_scope('Weights_Biases'):
+            # Weight matrices.
+            self.w = tf.get_variable(
+                'W',
+                shape=[4, self.vocab_size, self.hidden_size],
+                initializer=tf.random_normal_initializer(stddev=0.01)
+            )
+
+            self.u = tf.get_variable(
+                'U',
+                shape=[4, self.vocab_size, self.hidden_size],
+                initializer=tf.random_normal_initializer(stddev=0.01)
+            )
+
+            self.v = tf.get_variable(
+                'V',
+                shape=[4, self.vocab_size, self.hidden_size],
+                initializer=tf.random_normal_initializer(stddev=0.01)
+            )
+
+            self.biases = tf.constant(
+                0.1,
+                shape=[1, self.vocab_size, self.hidden_size],
+                name='Biases'
+            )
+
+            tf.summary.histogram('W', self.w)
+            tf.summary.histogram('Biases', self.biases)
+            tf.summary.histogram('U', self.u)
+            tf.summary.histogram('V', self.v)
+
         def define_placeholders():
             """
             Defines the placeholders for the LSTM network.
@@ -47,17 +78,55 @@ class LSTM(object):
 
         define_placeholders()
 
-    def hidden_layer(self, name='Input Layer'):
-        # defines the hidden layer for the LSTM network
-        with tf.name_scope(name):
-            weights = tf.get_variable(
-                'weights',
-                shape=[self.vocab_size, self.hidden_size],
-                initializer=tf.random_normal_initializer(stddev=0.01)
-            )
+    def step(self, prev, current):
+        """
+        Operation to perform while scanning
+        :param prev: 
+        :param current: 
+        :return: 
+        """
+        ht_1, ct_1 = tf.unstack(prev)
 
-            biases = tf.get_variable(
-                'biases',
-                shape=[self.vocab_size, self.hidden_size],
-                initializer=tf.random_normal_initializer(stddev=0.01)
-            )
+        # Forget gate
+        forget_gate = tf.sigmoid(
+            tf.matmul(
+                current, self.w[0]
+            ) + tf.matmul(
+                ht_1, self.u[0]
+            ) + self.biases
+        )
+
+        # Input gate
+        input_gate = tf.sigmoid(
+            tf.matmul(
+                current, self.w[1]
+            ) + tf.matmul(
+                ht_1, self.u[1]
+            ) + self.biases
+        )
+
+        # Candidate gate
+        candidate_gate = tf.tanh(
+            tf.matmul(
+                current, self.w[2]
+            ) + tf.matmul(
+                ht_1, self.u[2]
+            ) + self.biases
+        )
+
+        # Output gate
+        output_gate = tf.sigmoid(
+            tf.matmul(
+                current, self.w[3]
+            ) + tf.matmul(
+                ht_1, self.u[3]
+            ) + tf.matmul(
+                candidate_gate, self.v[3]
+            ) + self.biases
+        )
+
+        ct = (ct_1 * forget_gate) + (candidate_gate * input_gate)
+
+        ht = tf.tanh(ct) * output_gate
+
+        return tf.stack([ht, ct])
