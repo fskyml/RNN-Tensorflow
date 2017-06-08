a10db59edc7caee43b4f6ce063d8290edc2c8557
Arkoprabho Chakraborti <achakraborti@live.com>
2017-06-08 12:06:35 +0530

    Added bi-directioanl-lstm.py

---
+++ bi-directional-lstm.py
@@ -0,0 +1,8 @@
+"""In this module we try to implement the bi-directional LSTM"""
+import tensorflow as tf
+
+class BiLSTM(object):
+    """Defines the model of the bi-directional LSTM"""
+
+    def __init__(self):
+        """Initializes a new instance of the BiLSTM class"""
